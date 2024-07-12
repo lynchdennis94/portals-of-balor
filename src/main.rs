3dd417de169a49a614c23ae9e1aeb5407b49c0be
Dennis Lynch <lynch.dennis94@gmail.com>
2024-07-11 22:00:20 -0400

    Implements Section 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,140 @@
-use bracket_lib::terminal::{BTerm, GameState};
+use bracket_lib::terminal::{BTerm, GameState, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 
-struct State {}
+struct State {
+    ecs: World,
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: bracket_lib::terminal::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct LeftMover {}
+
+struct LeftWalker {}
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 79;
+            }
+        }
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut BTerm) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left | VirtualKeyCode::H | VirtualKeyCode::Numpad4 => {
+                try_move_player(-1, 0, &mut gs.ecs)
+            }
+            VirtualKeyCode::Up | VirtualKeyCode::K | VirtualKeyCode::Numpad8 => {
+                try_move_player(0, -1, &mut gs.ecs)
+            }
+            VirtualKeyCode::Right | VirtualKeyCode::L | VirtualKeyCode::Numpad6 => {
+                try_move_player(1, 0, &mut gs.ecs)
+            }
+            VirtualKeyCode::Down | VirtualKeyCode::J | VirtualKeyCode::Numpad2 => {
+                try_move_player(0, 1, &mut gs.ecs)
+            }
+            VirtualKeyCode::Y | VirtualKeyCode::Numpad7 => try_move_player(-1, -1, &mut gs.ecs),
+            VirtualKeyCode::U | VirtualKeyCode::Numpad9 => try_move_player(1, -1, &mut gs.ecs),
+            VirtualKeyCode::B | VirtualKeyCode::Numpad1 => try_move_player(-1, 1, &mut gs.ecs),
+            VirtualKeyCode::N | VirtualKeyCode::Numpad3 => try_move_player(1, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad5 => try_move_player(0, 0, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 fn main() -> bracket_lib::terminal::BError {
     use bracket_lib::terminal::BTermBuilder;
     let context: BTerm = BTermBuilder::simple80x50()
         .with_title("Portals of Balor")
         .build()?;
-    let gs: State = State {};
+    let mut gs: State = State { ecs: World::new() };
+
+    // Register components to the world
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
+    // Create some sample entities
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: 0x0040,
+            fg: RGB::named(bracket_lib::terminal::YELLOW),
+            bg: RGB::named(bracket_lib::terminal::BLACK),
+        })
+        .with(Player {})
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: 0x0001,
+                fg: RGB::named(bracket_lib::terminal::RED),
+                bg: RGB::named(bracket_lib::terminal::BLACK),
+            })
+            .with(LeftMover {})
+            .build();
+    }
+
     bracket_lib::terminal::main_loop(context, gs)
 }
