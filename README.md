899c6133620d772aeb206893624f66cce55c3454
Dennis Lynch <lynch.dennis94@gmail.com>
2024-07-13 14:37:17 -0400

    Implementation for Section 2.2

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ Week One - Sections 2.0 and 2.1
 * Implemented additional keycodes for alternative movement schemes. I typically like using the numpad scheme (including using 5 as a no-op/rest button), and if I'm developing on a laptop without a keypad I want to have the vi-keys available for the full range of motion. The arrow keys implemented in the tutorial are fine, but limits the motion and makes me feel as though I'm getting only a portion of the game
 * Added a tile size to the builder to create a bigger playing surface - the default was particularly small
 * I'm building this in part to follow off of the tutorial for Rust; I'd also like to expand the gameplay mechanics and pull from some games I enjoy (Dark Souls/Elden Ring, The Elder Scrolls, Diablo II) and put these mechanics in a setting I'd find interesting (fantasy influenced by Celtic mythology). Hence the name! While the initial implementation will hew closely to the tutorial, I hope to branch out over time and incorporate these additional elements - so in future weeks, there may be some level of divergence
+
+Week Two - Sections 2.2 and 2.3
+----
+* At some point I know the tutorial will clean up the 'magic numbers', but they're already annoying me; I've added the constants for map height and width and cleaned up references to these values that were input directly. I've also updated the terminal builder to use these constants directly
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use bracket_lib::terminal::{BTerm, GameState, VirtualKeyCode, RGB};
+use bracket_lib::terminal::{BError, BTerm, GameState, VirtualKeyCode, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
+
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 50;
 
 struct State {
     ecs: World,
@@ -13,6 +16,9 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -21,10 +27,9 @@ impl GameState for State {
         }
     }
 }
+
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -42,33 +47,22 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
-struct LeftWalker {}
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
 #[derive(Component, Debug)]
 struct Player {}
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let possible_x = pos.x + delta_x;
+        let possible_y = pos.y + delta_y;
+        let destination_idx = xy_idx(possible_x, possible_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(MAP_WIDTH - 1, max(0, possible_x));
+            pos.y = min(MAP_HEIGHT - 1, max(0, possible_y));
+        }
     }
 }
 
@@ -98,9 +92,78 @@ fn player_input(gs: &mut State, ctx: &mut BTerm) {
     }
 }
 
-fn main() -> bracket_lib::terminal::BError {
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * MAP_WIDTH as usize) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; (MAP_WIDTH * MAP_HEIGHT) as usize];
+
+    for x in 0..MAP_WIDTH {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, MAP_HEIGHT - 1)] = TileType::Wall;
+    }
+
+    for y in 0..MAP_HEIGHT {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(MAP_WIDTH - 1, y)] = TileType::Wall;
+    }
+
+    // Get RNG to splatter some walls around!
+    let mut rng = bracket_lib::random::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, MAP_WIDTH - 1);
+        let y = rng.roll_dice(1, MAP_HEIGHT - 1);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut BTerm) {
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 0.5, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                0x2E,
+            ),
+            TileType::Wall => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.0, 1.0, 0.0),
+                RGB::from_f32(0., 0., 0.),
+                0x23,
+            ),
+        }
+
+        x += 1;
+        if x >= MAP_WIDTH {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
+fn main() -> BError {
     use bracket_lib::terminal::BTermBuilder;
-    let context: BTerm = BTermBuilder::simple80x50()
+    let context: BTerm = BTermBuilder::simple(MAP_WIDTH, MAP_HEIGHT)
+        .expect("Could not construct BTermBuilder")
         .with_title("Portals of Balor")
         .with_tile_dimensions(16, 16)
         .build()?;
@@ -109,33 +172,22 @@ fn main() -> bracket_lib::terminal::BError {
     // Register components to the world
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+
+    // Add shared data for the world
+    gs.ecs.insert(new_map());
 
     // Create some sample entities
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
         .with(Renderable {
-            glyph: 0x0040,
+            glyph: 0x40,
             fg: RGB::named(bracket_lib::terminal::YELLOW),
             bg: RGB::named(bracket_lib::terminal::BLACK),
         })
         .with(Player {})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: 0x0001,
-                fg: RGB::named(bracket_lib::terminal::RED),
-                bg: RGB::named(bracket_lib::terminal::BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    }
 
     bracket_lib::terminal::main_loop(context, gs)
 }
